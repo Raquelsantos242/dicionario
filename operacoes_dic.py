8d92b5c39097004fbced2cf85619d58da7764108
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-09-15 16:39:42 -0300

    Add files via upload

---
+++ operacoes_dic.py
@@ -0,0 +1,13 @@
+dic = {'mae': 'yoko', 'pai': 'john', 'filho': 'sean'}
+
+print(dic['mae'])
+print(dic['pai'])
+
+dic["enteado"] = "Julian" #inseri um elemento
+dic["pai"] = "John Lennon" #alterei um elemento
+
+print(dic)
+
+print("mae" in dic) #true
+print("primo" in dic) #false
+print(len(dic)) #total de elementos
