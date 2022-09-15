535bc680b85ec7b8bb5ab4fc27b72600eb7e17aa
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-09-15 16:25:34 -0300

    Add files via upload

---
+++ cria_dic.py
@@ -0,0 +1,18 @@
+dic_alunos = {"M09": "Bob",
+              "M01": "Dayse",
+              "M13": "Jill",
+              "M05": "Cris",
+              "M19": "Jimi"}
+
+print(dic_alunos)
+print(dic_alunos['M13']) #acessamos os elementos pela chave no dicionário
+
+#aqui é uma lista
+list_alunos = ("bob", "Dayse", "jill", "cris", "jimi")
+
+print(list_alunos)
+print(list_alunos[2]) #acessamos os elementos pela opsição na lista
+
+#outros dicionários
+dic_titulos = {'portela': 22, 'mangueira':19, 'beija-flor': 14}
+dic_vazio = dict()
